41c87afa8bd5b96dfd1735f160299ee49636cdd6
Youssef <yelabd@purdue.edu>
2016-12-17 21:37:24 +0200

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by Youssef Elabd on 12/17/16.
+//  Copyright © 2016 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
