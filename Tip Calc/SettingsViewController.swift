5d62f11a8507478b19bce34ac1543e042594becd
Youssef <yelabd@purdue.edu>
2017-01-06 04:49:00 +0200

    Initial Commit

---
+++ Tip Calc/SettingsViewController.swift
@@ -0,0 +1,72 @@
+//
+//  SettingsViewController.swift
+//  Tip Calc
+//
+//  Created by Youssef Elabd on 12/18/16.
+//  Copyright © 2016 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+   
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+
+    @IBAction func defaultChanged(_ sender: Any) {
+        let defaults = UserDefaults.standard
+       
+        
+        defaults.set(segmentedControl.selectedSegmentIndex, forKey: "defaultTip")
+        defaults.synchronize()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        let startIndex = defaults.integer(forKey: "defaultTip")
+        segmentedControl.selectedSegmentIndex = startIndex
+        //print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+         
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tip Calc/ViewController.swift
+++ Tip Calc/ViewController.swift
@@ -10,8 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipMultiplier: UISegmentedControl!
+    @IBOutlet weak var tip2: UILabel!
+    @IBOutlet weak var tip3: UILabel!
+    @IBOutlet weak var tip4: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        billField.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +31,70 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        
+        defaults.set(0, forKey: "initialized")
+        
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip  = bill * tipPercentages[tipMultiplier.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        tip2.text = String(format: "$%.2f", tip/2)
+        tip3.text = String(format: "$%.2f", tip/3)
+        tip4.text = String(format: "$%.2f", tip/4)
+    }
+    
+    @IBAction func calculateTip2(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        
+        let intDefault = defaults.integer(forKey: "defaultTip")
+        
+        tipMultiplier.selectedSegmentIndex = intDefault
+
+        let tipPercentages = [0.18,0.2,0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip  = bill * tipPercentages[intDefault]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        tip2.text = String(format: "$%.2f", tip/2)
+        tip3.text = String(format: "$%.2f", tip/3)
+        tip4.text = String(format: "$%.2f", tip/4)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        calculateTip2(self)
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
 }
 
